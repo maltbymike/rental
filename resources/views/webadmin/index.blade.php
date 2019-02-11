9c05ae1c4c7a017cc47a61b82f9b71770d6a1fdc
Mike Maltby <maltby.mike@gmail.com>
2019-02-10 23:40:57 -0500

    Added Admin Home to menue and moved the top-navbar to a partial file

--- resources/views/webadmin/index.blade.php
+++ resources/views/webadmin/index.blade.php
@@ -12,12 +12,21 @@
       <div class="card-header">Inventory Tools</div>
       <div class="card-body">
         <div class="row">
+
           <div class="text-center col-sm-3">
             <a href="/product/category/">
               <img src="{{ asset('img/icons/Tools.png') }}">
               <div>Product Categories</div>
             </a>
           </div>
+
+          <div class="text-center col-sm-3">
+            <a href="/product/category/create">
+              <img src="{{ asset('img/icons/add.png') }}">
+              <div>Add New Product Categories</div>
+            </a>
+          </div>
+
         </div>
       </div>
   </div>
