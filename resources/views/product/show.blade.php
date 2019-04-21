fce47ef28a8de2defcbb7ed78f51f232ed25ac98
Mike Mlatby <maltby.mike@gmail.com>
2019-04-20 23:27:30 -0400

    Change product page layout

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -24,57 +24,9 @@
 
   <div class="row">
 
-    <!-- Begin Rates Table -->
-    <div class="col-sm-6 col-lg-3 order-lg-12 mb-4">
-      <table class="table table-hover">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="rowgroup" colspan="2">Rental Rates</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row" style="width: 50%">4 Hours</th>
-            <td class="text-right" style="width: 50%">{{ $product->rates->firstWhere('hours', ">=", env('4_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('4_HOURS'))->rate : "---" }}</td>
-          </tr>
-          <tr>
-            <th scope="row">Daily</th>
-            <td class="text-right">{{ $product->rates->firstWhere('hours', ">=", env('DAY_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('DAY_HOURS'))->rate : "---" }}</td>
-          </tr>
-          <tr>
-            <th scope="row">Weekly</th>
-            <td class="text-right">{{ $product->rates->firstWhere('hours', ">=", env('WEEK_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('WEEK_HOURS'))->rate : "---" }}</td>
-          </tr>
-        </tbody>
-        <thead class="thead-light">
-          <tr>
-            <th scope="rowgroup" colspan="2">Weekend Rates</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">Out Friday after 4pm</th>
-            <td class="text-right">
-              @if ($product->rates->where('hours', env('DAY_HOURS'))->first())
-                {{ number_format($product->rates->where('hours', env('DAY_HOURS'))->first()->rate * 1.5, 2) }}
-              @elseif ($product->rates->firstWhere('hours', ">=", env('WEEK_HOURS')))
-                {{ $product->rates->firstWhere('hours', ">=", env('WEEK_HOURS'))->rate }}
-              @else
-                ---
-              @endif
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Out Saturday</th>
-            <td class="text-right">{{ $product->rates->firstWhere('hours', ">=", env('DAY_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('DAY_HOURS'))->rate : "---" }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <!-- End Rates Table -->
-
     <!-- Begin Image Card -->
-    <div class="col-sm-6 col-lg-4 mb-4">
+    <div class="col-md-6 mb-4">
+    <!-- <div class="col-sm-6 col-lg-4 mb-4"> -->
       <div class="card">
         <div id="productImageCarousel" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
@@ -91,12 +43,14 @@
                 @else
                   <div class="carousel-item">
                 @endif
-                    <img class="d-block w-100" src="{{ asset('/storage/images/' . $image->filename) }}" alt="Image of {{ $product->name }}" />
+                    <img class="h-100 d-block mx-auto" src="{{ asset('/storage/images/' . $image->filename) }}" alt="Image of {{ $product->name }}" />
                   </div>
                 <?php $i++; ?>
               @endforeach
             @else
-              <img class="d-block w-100" src="https://via.placeholder.com/1200x1200?text=Image+Coming+Soon" />
+              <div class="carousel-item active">
+                <img class="h-100 d-block mx-auto" src="https://via.placeholder.com/1200x1200?text=Image+Coming+Soon" />
+              </div>
             @endif
           </div>
           <a class="carousel-control-prev" href="#productImageCarousel" role="button" data-slide="prev">
@@ -113,35 +67,66 @@
     </div>
     <!-- End Image Card -->
 
+    <div class="col-md-6 mb-4">
 
-    <!-- Begin Product Content Section -->
-    <div class="col-12 col-lg-5 mb-4">
-      <h4>{{ $product->name }}</h4>
-      <p>{{ $product->description }}</p>
+      <!-- Begin Rates Table -->
+      <div class="row mb-4 align-items-end" style="font-size: larger">
+        <h4 class="col-12 col-lg-7 p-3 mt-lg-0 mt-2 mb-0 bg-dark text-light">Rental Rates:</h4>
 
-      @if ($product->part_number)
-      <dl class="row">
-        <dt class="col-sm-4">Product Number</dt>
-        <dd class="col-sm-8">{{ $product->part_number }}</dd>
-      </dl>
-      @endif
+        <p class="order-lg-1 col-7 col-lg d-lg-block d-inline text-lg-center mb-0 p-2 align-bottom">4 Hours</p>
+        <p class="btn btn-lg btn-primary order-lg-3 col-5 col-lg mx-lg-1 my-1">{{ $product->rates->firstWhere('hours', ">=", env('4_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('4_HOURS'))->rate : "---" }}</p>
 
-      @if ($product->manufacturer)
-      <dl class="row">
-        <dt class="col-sm-4">Manufacturer</dt>
-        <dd class="col-sm-8">{{ $product->manufacturer->name }}</dd>
-      </dl>
-      @endif
+        <p class="order-lg-1 col-7 col-lg d-lg-block d-inline text-lg-center mb-0 p-2">Daily</p>
+        <p class="btn btn-lg btn-primary order-lg-3 col-5 col-lg mx-lg-1 my-1">{{ $product->rates->firstWhere('hours', ">=", env('DAY_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('DAY_HOURS'))->rate : "---" }}</p>
 
-      @if ($product->model)
-      <dl class="row">
-        <dt class="col-sm-4">Model</dt>
-        <dd class="col-sm-8">{{ $product->model }}</dd>
-      </dl>
-      @endif
+        <p class="order-lg-1 col-7 col-lg d-lg-block d-inline text-lg-center mb-0 p-2">Weekly</p>
+        <p class="btn btn-lg btn-primary order-lg-3 col-5 col-lg mx-lg-1 my-1">{{ $product->rates->firstWhere('hours', ">=", env('WEEK_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('WEEK_HOURS'))->rate : "---" }}</p>
+
+        <h4 class="col-12 col-lg-5 mt-lg-0 mt-2 mb-0 p-3 bg-secondary text-lg-center text-light">Weekend Rates</h5>
+
+        <p class="order-lg-1 col-7 col-lg d-lg-block d-inline text-lg-center mb-0 p-2">Out Friday After 4pm</p>
+        <p class="btn btn-lg btn-primary order-lg-3 col-5 col-lg mx-lg-1 my-1">
+            @if ($product->rates->where('hours', env('DAY_HOURS'))->first())
+              {{ number_format($product->rates->where('hours', env('DAY_HOURS'))->first()->rate * 1.5, 2) }}
+            @elseif ($product->rates->firstWhere('hours', ">=", env('WEEK_HOURS')))
+              {{ $product->rates->firstWhere('hours', ">=", env('WEEK_HOURS'))->rate }}
+            @else
+              ---
+            @endif
+        </p>
+
+        <p class="order-lg-1 col-7 col-lg d-lg-block d-inline text-lg-center mb-0 p-2">Out Saturday</p>
+        <p class="btn btn-lg btn-primary order-lg-3 col-5 col-lg mx-lg-1 my-1">{{ $product->rates->firstWhere('hours', ">=", env('DAY_HOURS')) ? $product->rates->firstWhere('hours', ">=", env('DAY_HOURS'))->rate : "---" }}</p>
+
+        <div class="order-lg-2 col-12 bg-dark"></div>
+
+      </div>
+      <!-- End Rates Table -->
+
+      <!-- Begin Product Content Section -->
+      <div class="row">
+        <h4 class="col-12 p-3 bg-dark text-light">{{ $product->name }}</h4>
+        <p class="col-12">{{ $product->description }}</p>
+
+        @if ($product->part_number)
+          <p class="col-4 font-weight-bold">Product Number</p>
+          <p class="col-8">{{ $product->part_number }}</p>
+        @endif
+
+        @if ($product->manufacturer)
+          <p class="col-sm-4 font-weight-bold">Manufacturer</p>
+          <p class="col-sm-8">{{ $product->manufacturer->name }}</p>
+        @endif
+
+        @if ($product->model)
+          <p class="col-sm-4 font-weight-bold">Model</p>
+          <p class="col-sm-8">{{ $product->model }}</p>
+        @endif
+
+      </div>
+      <!-- End Product Content Section  -->
+
     </div>
-    <!-- End Product Content Section  -->
-
 
   </div>
 
